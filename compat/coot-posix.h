125b050f9a99d446a21f60f50c02e57d9f2bd63f
Michal Malý <madcatxster@devoid-pointer.net>
2022-10-25 19:15:06 +0200

    Fix Posix build

---
+++ compat/coot-posix.h
@@ -0,0 +1,16 @@
+// vim: sw=4 ts=4 sts=4 expandtab :
+
+#ifndef COMPAT_COOT_POSIX_H
+#define COMPAT_COOT_POSIX_H
+
+#include <string>
+
+namespace coot {
+namespace sysdep {
+
+	// Empty
+
+} // namespace sysdep
+} // namespace coot
+
+#endif // COMPAT_COOT_POSIX_H
