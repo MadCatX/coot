8c6f604931b8d8124475c95edfad02356241d471
Michal Malý <madcatxster@devoid-pointer.net>
2022-10-31 11:32:43 +0100

    Do not call stat() directly in src/molecule-class-info-maps.cc

--- src/updating-map-params.hh
+++ src/updating-map-params.hh
@@ -8,8 +8,7 @@ class updating_map_params_t {
 public:
    updating_map_params_t() {
       imol = -1;
-      ctime.tv_sec = 0;
-      ctime.tv_nsec = 0;
+      ctime = 0;
       is_difference_map = false;
       use_weights = false;
    }
@@ -22,12 +21,10 @@ public:
       mtz_file_name(mtz_file_name_in), f_col(f_col_in), phi_col(phi_col_in), weight_col(w_col_in)
    {
       imol = imol_in;
-      ctime.tv_sec = 0;
-      ctime.tv_nsec = 0;
+      ctime = 0;
       use_weights = use_weights_in;
       is_difference_map = is_difference_map_in;
-      ctime.tv_sec = 0;
-      ctime.tv_nsec = 0;
+      ctime = 0;
    }
    int imol;
    std::string mtz_file_name;
@@ -36,7 +33,7 @@ public:
    std::string weight_col;
    bool use_weights;
    bool is_difference_map;
-   timespec ctime;
+   uint_least64_t ctime;
 };
 
 #endif // UPDATING_MAP_PARAMS_T_HH
